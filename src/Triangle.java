639f45ec5660ae47f6e6c611c06764ac2deaecb0
AlexandrStopkin <stnalex1@gmail.com>
2023-03-09 14:53:44 +0100

    Add add fields

--- src/Triangle.java
+++ src/Triangle.java
@@ -13,8 +13,15 @@
 //
 //    При провале любой из проверок нужно вывести на экран соответствующее сообщение.
 public class Triangle {
-  public Triangle(int a, int b, int c) {
+  private int a;
+  private int b;
+  private int c;
 
+  public Triangle(int a, int b, int c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
 
   }
+
 }
