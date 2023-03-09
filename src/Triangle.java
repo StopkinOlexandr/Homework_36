3061c5191f49032e87b3d9cde57ec7cb236e97d7
AlexandrStopkin <stnalex1@gmail.com>
2023-03-09 16:09:44 +0100

    Add methods checkSides() and printTriangleCheck()

--- src/Triangle.java
+++ src/Triangle.java
@@ -17,11 +17,31 @@ public class Triangle {
   private int b;
   private int c;
 
+
   public Triangle(int a, int b, int c) {
     this.a = a;
     this.b = b;
     this.c = c;
-
+    String result;
+    if (a <= 0 || b <= 0 || c <= 0) {
+      result = "Incorrect sides dimensions";
+      printTriangleCheck(result);
+    } else {
+      result = checkSides(a, b, c);
+      printTriangleCheck(result);
+    }
   }
 
+  public static String checkSides(int a, int b, int c) {
+    if (a + b < c || b + c < a || a + c < b) {
+      return "Triangle is unequal";
+    }
+    return null;
+  }
+
+  public static void printTriangleCheck(String res) {
+    if (res != null) {
+      System.out.println(res);
+    }
+  }
 }
